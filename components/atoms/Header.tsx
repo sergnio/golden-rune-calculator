c68f5290e57eff40682a1f5b0bc22dcb111e950d
sergnio <sergnioa@gmail.com>
2022-03-23 18:39:17 -0500

    buncha changes

---
+++ components/atoms/Header.tsx
@@ -0,0 +1,13 @@
+export default () => (
+  <header>
+    <ul>
+      Header
+      {/*<Link activeClassName="active" href={Routes.home}>*/}
+      {/*  simple calculator*/}
+      {/*</Link>*/}
+      {/*<Link activeClassName="active" href={Routes.advancedRouteHeldRunesEntry}>*/}
+      {/*  advanced calculator*/}
+      {/*</Link>*/}
+    </ul>
+  </header>
+);
