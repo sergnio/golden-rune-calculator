26ec0873a7032f88f64dfe2dab4e698e97268895
Stephen Cronin <cronin4392@gmail.com>
2022-05-22 14:51:57 -0400

    Refactor table to use grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import styles from "../styles/Home.module.css";
-import SoulCounter from "../components/atoms/SoulCounter/SoulCounter";
+import SoulCounter from "../components/atoms/SoulCounter";
 import useSoulCounter from "../components/atoms/SoulCounter/useSoulCounter";
 
 export default () => {
   const props = useSoulCounter();
 
   return (
-    <div>
-      <main className={styles.main}>
-        <SoulCounter {...props} />
-      </main>
+    <div className={styles.main}>
+      <SoulCounter {...props} />
     </div>
   );
 };
