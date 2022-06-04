86243e27b58e706ee9a8558eacc8d157cbefd940
Stephen Cronin <cronin4392@gmail.com>
2022-06-04 18:29:30 -0400

    New Button styles using SCSS extend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import SoulCounter from "../components/atoms/SoulCounter";
-import useSoulCounter from "../components/atoms/SoulCounter/useSoulCounter";
+import SoulCounter from "@/components/atoms/SoulCounter";
+import useSoulCounter from "@/components/atoms/SoulCounter/useSoulCounter";
 
-export default () => {
+const Index = () => {
   const props = useSoulCounter();
   return <SoulCounter {...props} />;
 };
+
+export default Index;
