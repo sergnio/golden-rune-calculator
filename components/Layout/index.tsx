cc17d3bd059bc834048b8358a1804fcd9b8325b3
sergnio <sergnioa@gmail.com>
2022-06-16 16:31:26 -0500

    Add back in google analytics tag

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -11,6 +11,25 @@ export default ({ children }: Props) => {
     <>
       <Head>
         <title>Golden Rune Calculator</title>
+        {/*  Global site tag (gtag.js) - Google Analytics */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KCTJK4JDN7"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-KCTJK4JDN7');
+                </script>`,
+          }}
+        />
+        {/* End global site tag (gtag.js) - Google Analytics */}
         <meta
           name="description"
           content="Golden Rune Calculator for Elden Ring"
