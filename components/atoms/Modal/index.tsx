d37321dadde664870acd3f7298e564a369ad31c1
Stephen Cronin <cronin4392@gmail.com>
2022-12-10 00:19:42 -0500

    Create a Modal component

---
+++ components/atoms/Modal/index.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
+import styles from "./styles.module.scss";
+
+type Props = {
+  title?: string;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+const Modal: React.FC<Props> = ({ open, setOpen, children, title }) => {
+  return (
+    <div className={styles.Overlay} data-open={open}>
+      <div className={styles.Content}>
+        <div className={styles.Header}>
+          <div>{title && <h1 className={styles.H1}>Summary</h1>}</div>
+          <button className={styles.CloseButton} onClick={() => setOpen(false)}>
+            Close
+          </button>
+        </div>
+        <>{children}</>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
