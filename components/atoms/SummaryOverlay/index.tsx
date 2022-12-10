0c1084ac3adfe1907335b6b01b0be11df9bb8d71
Stephen Cronin <cronin4392@gmail.com>
2022-12-10 00:19:42 -0500

    Create a Modal component

--- components/atoms/SummaryOverlay/index.tsx
+++ components/atoms/SummaryOverlay/index.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import { SoulCounterReturn } from "@/components/atoms/SoulCounter/useSoulCounter";
+import Modal from "@/components/atoms/Modal";
 import { getOverUnder, OverUnder } from "@/utils/calculate";
 import styles from "./styles.module.scss";
 
@@ -44,46 +45,36 @@ const SummaryOverlay: React.FC<Props> = ({
   const overUnder = getOverUnder(totalNeeded);
 
   return (
-    <div className={styles.Overlay} data-open={open}>
-      <div className={styles.Content}>
-        <div className={styles.Header}>
-          <div>
-            <h1 className={styles.H1}>Summary</h1>
-          </div>
-          <button className={styles.CloseButton} onClick={() => setOpen(false)}>
-            Close
-          </button>
-        </div>
-        <div className={styles.CountSection}>
-          <p className={styles.P}>
-            You have {held} runes and need {needed} runes.
-          </p>
-        </div>
+    <Modal open={open} setOpen={setOpen} title="Summary">
+      <div className={styles.CountSection}>
+        <p className={styles.P}>
+          You have {held} runes and need {needed} runes.
+        </p>
+      </div>
 
-        <div className={styles.CountSection}>
-          <p className={styles.P}>If you consume:</p>
-          <div className={styles.Counts}>
-            {runeCountHeld.length > 0 ? (
-              <>
-                {runeCountHeld.map((rune) => (
-                  <div key={rune.id} className={styles.Count}>
-                    {rune.count} x {rune.label}
-                  </div>
-                ))}
-              </>
-            ) : (
-              <div className={styles.Count}>No runes</div>
-            )}
-          </div>
-        </div>
-
-        <div className={styles.CountSection}>
-          <p className={styles.P} data-over-under={overUnder}>
-            {overUnderText(totalNeeded, overUnder)}
-          </p>
+      <div className={styles.CountSection}>
+        <p className={styles.P}>If you consume:</p>
+        <div className={styles.Counts}>
+          {runeCountHeld.length > 0 ? (
+            <>
+              {runeCountHeld.map((rune) => (
+                <div key={rune.id} className={styles.Count}>
+                  {rune.count} x {rune.label}
+                </div>
+              ))}
+            </>
+          ) : (
+            <div className={styles.Count}>No runes</div>
+          )}
         </div>
       </div>
-    </div>
+
+      <div className={styles.CountSection}>
+        <p className={styles.P} data-over-under={overUnder}>
+          {overUnderText(totalNeeded, overUnder)}
+        </p>
+      </div>
+    </Modal>
   );
 };
 
--- components/atoms/ValueOverlay/index.tsx
+++ components/atoms/ValueOverlay/index.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { SoulCounterReturn } from "@/components/atoms/SoulCounter/useSoulCounter";
 import NumberInput from "@/components/atoms/NumberInput";
+import Modal from "@/components/atoms//Modal";
 import styles from "./styles.module.scss";
 
 type Props = {
@@ -26,55 +27,50 @@ const ValueOverlay: React.FC<Props> = ({
   }, [held, needed]);
 
   return (
-    <div className={styles.Overlay} data-open={open}>
-      <div className={styles.Content}>
-        <button className={styles.CloseButton} onClick={() => setOpen(false)}>
-          Close
+    <Modal open={open} setOpen={setOpen}>
+      <form
+        className={styles.Form}
+        onSubmit={(event) => {
+          event.preventDefault();
+          setHeld(localHeld);
+          setNeeded(localNeeded);
+          setOpen(false);
+        }}
+      >
+        <fieldset className={styles.Fieldset}>
+          <label className={styles.Label} htmlFor="input-held">
+            Runes Held
+          </label>
+          <NumberInput
+            id="input-held"
+            className={styles.Input}
+            onChange={(event) =>
+              setLocalHeld(parseInt(event.target.value) || 0)
+            }
+            value={localHeld}
+          />
+        </fieldset>
+        <fieldset className={styles.Fieldset}>
+          <label className={styles.Label} htmlFor="input-needed">
+            Runes Needed
+          </label>
+          <NumberInput
+            id="input-needed"
+            className={styles.Input}
+            onChange={(event) =>
+              setLocalNeeded(parseInt(event.target.value) || 0)
+            }
+            value={localNeeded}
+          />
+        </fieldset>
+        <div className={styles.Message} data-visible={haveEnough}>
+          Looks like you have enough runes already!
+        </div>
+        <button className={styles.SubmitButton} type="submit">
+          Submit
         </button>
-        <form
-          className={styles.Form}
-          onSubmit={(event) => {
-            event.preventDefault();
-            setHeld(localHeld);
-            setNeeded(localNeeded);
-            setOpen(false);
-          }}
-        >
-          <fieldset className={styles.Fieldset}>
-            <label className={styles.Label} htmlFor="input-held">
-              Runes Held
-            </label>
-            <NumberInput
-              id="input-held"
-              className={styles.Input}
-              onChange={(event) =>
-                setLocalHeld(parseInt(event.target.value) || 0)
-              }
-              value={localHeld}
-            />
-          </fieldset>
-          <fieldset className={styles.Fieldset}>
-            <label className={styles.Label} htmlFor="input-needed">
-              Runes Needed
-            </label>
-            <NumberInput
-              id="input-needed"
-              className={styles.Input}
-              onChange={(event) =>
-                setLocalNeeded(parseInt(event.target.value) || 0)
-              }
-              value={localNeeded}
-            />
-          </fieldset>
-          <div className={styles.Message} data-visible={haveEnough}>
-            Looks like you have enough runes already!
-          </div>
-          <button className={styles.SubmitButton} type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+      </form>
+    </Modal>
   );
 };
 
