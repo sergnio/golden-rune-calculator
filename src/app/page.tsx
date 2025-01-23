c7d698e5f04976d390a8a6dc1cd2bc40f49bc8e9
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2025-01-23 17:03:26 -0500

    Super basic page switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,76 @@
+"use client";
+
+import { Dispatch, SetStateAction, useState } from "react";
+
+type Pages = "enter-runes" | "count-runes" | "summary";
+
 export default function Home() {
+  const [page, setPage] = useState<Pages>("enter-runes");
+
   return (
     <div>
       <main>
-        Elden Ring
+        {page === "enter-runes" && (
+          <EnterRunes nextPage={() => setPage("count-runes")} />
+        )}
+        {page === "count-runes" && (
+          <CountRunes
+            prevPage={() => setPage("enter-runes")}
+            nextPage={() => setPage("summary")}
+          />
+        )}
+        {page === "summary" && (
+          <Summary prevPage={() => setPage("count-runes")} />
+        )}
       </main>
-      <footer>
-        
-      </footer>
     </div>
   );
 }
+
+const EnterRunes = ({ nextPage }: { nextPage: () => void }) => {
+  return (
+    <div>
+      <div>Enter Runes</div>
+      <Pagination nextPage={nextPage} />
+    </div>
+  );
+};
+
+const CountRunes = ({
+  prevPage,
+  nextPage,
+}: {
+  nextPage: () => void;
+  prevPage: () => void;
+}) => {
+  return (
+    <div>
+      <div>Count Runes</div>
+      <Pagination prevPage={prevPage} nextPage={nextPage} />
+    </div>
+  );
+};
+
+const Summary = ({ prevPage }: { prevPage: () => void }) => {
+  return (
+    <div>
+      <div>Summary</div>
+      <Pagination prevPage={prevPage} />
+    </div>
+  );
+};
+
+const Pagination = ({
+  prevPage,
+  nextPage,
+}: {
+  nextPage?: () => void;
+  prevPage?: () => void;
+}) => {
+  return (
+    <div>
+      {prevPage ? <button onClick={prevPage}>Prev</button> : <div />}
+      {nextPage ? <button onClick={nextPage}>Next</button> : <div />}
+    </div>
+  );
+};
