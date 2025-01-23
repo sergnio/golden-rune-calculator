591edae4ce0045adb206611d2421af72cc035a42
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2025-01-23 18:47:29 -0500

    Clean up type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import { RuneCalc } from "@/components/RuneCalc";
 import { Screens } from "@/components/Screens";
 
-type Screens = "enter-runes" | "count-runes" | "summary";
-
 export default function Home() {
   return (
     <RuneCalc>
