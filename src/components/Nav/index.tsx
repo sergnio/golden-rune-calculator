61dcda50b94033906bd7d1f9628b83ecd6594f19
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2025-01-23 22:06:42 -0500

    Tweak gradient

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,20 +4,18 @@ import styles from "./styles.module.scss";
 
 export const Nav = () => (
   <nav className={styles.Nav}>
-    <div className={styles.Inner}>
-      <div className={styles.Logo}>
-        <Image src="/lordrune.png" alt="golden rune" height={80} width={80} />
-      </div>
-      <div className={styles.Links}>
-        <a
-          target="_blank"
-          href="https://github.com/sergnio/golden-rune-calculator"
-          rel="noreferrer"
-          className={styles.Contribute}
-        >
-          <Github /> Contribute
-        </a>
-      </div>
+    <div className={styles.Logo}>
+      <Image src="/lordrune.png" alt="golden rune" height={80} width={80} />
+    </div>
+    <div className={styles.Links}>
+      <a
+        target="_blank"
+        href="https://github.com/sergnio/golden-rune-calculator"
+        rel="noreferrer"
+        className={styles.Contribute}
+      >
+        <Github /> Contribute
+      </a>
     </div>
   </nav>
 );
